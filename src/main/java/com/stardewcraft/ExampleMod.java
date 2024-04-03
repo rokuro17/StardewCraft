b534c7f7643e17c9ed5df9d9635dd2fec4bb4a34
Rokuro <rokuro17red@gmail.com>
2024-04-03 15:35:01 +0200

    Update ExampleMod.java
    Added edibility to Stardrop

--- src/main/java/com/stardewcraft/ExampleMod.java
+++ src/main/java/com/stardewcraft/ExampleMod.java
@@ -3,12 +3,15 @@ package com.stardewcraft;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Set;
 
 public class ExampleMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
@@ -18,7 +21,14 @@ public class ExampleMod implements ModInitializer {
 
 	public static final Item STARDROP =
 			Registry.register(Registries.ITEM, new Identifier("stardewcraft", "stardrop"),
-					new Item(new FabricItemSettings()));
+					new Item(new FabricItemSettings().maxCount(13).food(new FoodComponent.Builder().alwaysEdible().hunger(10).saturationModifier(0.5f).build())));
+	public class Stardrop extends Item{
+		public Stardrop(Settings settings){
+			super(settings);
+		}
+	}
+
+
 
 	@Override
 	public void onInitialize() {
