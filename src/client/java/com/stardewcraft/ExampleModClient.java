746c3208320291e18701f4503c1d1ca1db786261
Rokuro <rokuro17red@gmail.com>
2024-04-03 15:01:08 +0200

    fixed modid vs stardewcraft
    Stardewcraft wins!

---
+++ src/client/java/com/stardewcraft/ExampleModClient.java
@@ -0,0 +1,16 @@
+package com.stardewcraft;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ExampleModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
--- src/main/java/com/stardewcraft/ExampleMod.java
+++ src/main/java/com/stardewcraft/ExampleMod.java
@@ -1,7 +1,12 @@
-package com.example;
+package com.stardewcraft;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,7 +14,11 @@ public class ExampleMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+    public static final Logger LOGGER = LoggerFactory.getLogger("stardewcraft");
+
+	public static final Item STARDROP =
+			Registry.register(Registries.ITEM, new Identifier("stardewcraft", "stardrop"),
+					new Item(new FabricItemSettings()));
 
 	@Override
 	public void onInitialize() {
