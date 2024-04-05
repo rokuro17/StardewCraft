4427e74877181571d25864b5042e8291f0cc2193
Rokuro <rokuro17red@gmail.com>
2024-04-05 17:47:56 +0200

    Translations!
    Traducciones!

--- src/main/java/com/stardewcraft/items/Stardrop.java
+++ src/main/java/com/stardewcraft/items/Stardrop.java
@@ -1,6 +1,7 @@
 package com.stardewcraft.items;
 
 import net.minecraft.item.Item;
+import net.minecraft.text.TranslatableTextContent;
 
 public class Stardrop extends Item {
     public Stardrop(Settings settings){
